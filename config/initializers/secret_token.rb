2112152a9f8766bced1d2a5c85b6ea5b92e78173
Zach Morek <zach@Zach-MBP.local>
2011-09-24 01:54:39 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskTicketer::Application.config.secret_token = '62e758e21f73832d94a4110ab67bc27fdbd381edb0fbeb9ebf06cca84bf30dcd71a285dc061fe272372e9113d86bc8dd934cd0f13b64faebafb6b2fe9970cc79'
