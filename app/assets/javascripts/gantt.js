3a9014509ee20e78f1eb6f0e09bccf87a74e65b2
Will Klein <will@Wills-MacBook-Air.local>
2011-09-24 21:09:43 -0400

    Progress on gantt.

--- app/assets/javascripts/gantt.js
+++ app/assets/javascripts/gantt.js
@@ -27,13 +27,13 @@ var tasks = [
 
 var real_tasks = [
     {
-        "assigned_date": "2011-09-24T19:08:00Z",
-        "closed_date": "2011-09-24T19:08:00Z",
+        "assigned_date": "2011-09-23T19:08:00Z",
+        "closed_date": "",
         "created_at": "2011-09-24T19:08:44Z",
-        "expectation_date": "2011-09-24T19:08:00Z",
+        "expectation_date": "2011-09-25T19:08:00Z",
         "id": 1,
         "name": "Test",
-        "opened_date": "2011-09-24T19:08:00Z",
+        "opened_date": "2011-09-23T19:08:00Z",
         "updated_at": "2011-09-24T19:08:44Z",
         "user_id": 1
     },
@@ -103,4 +103,43 @@ $(function() {
     });
     
     $(".gantt").disableSelection();
+    
+    var realTaskLength = real_tasks.length,
+        ;
+    
+    for (i = 0; i < realTaskLength; i++) {
+        task = real_tasks[i];
+        
+        task.assigned_date = UTIL.cleanDate(task.assigned_date);
+        task.expectation_date = UTIL.cleanDate(task.expectation_date);
+        task.opened_date = UTIL.cleanDate(task.opened_date);
+        task.closed_date = UTIL.cleanDate(task.closed_date);
+        
+        var diffDate = task.closed_date.getTime() - task.assigned_date.getTime();
+        
+        alert('This task took ' + (diffDate / 1000 * 60) + ' hours to complete from assignment.');
+    }
 });
+
+var UTIL = {
+    /* expected input: '2011-09-24T19:08:44Z' */
+    cleanDate = function(dirtyDate) {
+        var year,
+            month,
+            day,
+            hour,
+            minute;
+    
+        if (dirtyDate.length < 16) {
+            return false;
+        }
+    
+        year = dirtyDate.substr(0, 4);
+        month = dirtyDate.substr(5, 2);
+        day = dirtyDate.substr(8, 2);
+        hour = dirtyDate.substr(11, 2);
+        minute = dirtyDate.substr(14, 2);
+    
+        return Date(year, month, day, hour, minute);
+    }
+};
